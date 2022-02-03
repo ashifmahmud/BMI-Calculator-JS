09ff1faebd3fcebb7e8aade4ea2d925e7ee4ae3c
ashifmahmud <ashifmahmudjoy@gmail.com>
2022-02-03 13:56:30 +0600

    4th commit

--- bmi.js
+++ bmi.js
@@ -19,26 +19,34 @@ function bmiCalculator(){
   bmi = kg/(newCm * newCm);
 
   bmi = bmi.toFixed(1);
+  
+  var uw = ["Under weight, eat healthy!","Don't fly away with wind!","Lighter than a feather!","Let's go eat something heavy!","Wanna fly?"];
+  var fit=["Wow! You are so fit!", "Thanks for being fit!","You are fit like a model!","Atta boy! So fit!","You rock!"];
+  var ow = ["Diet and workout is fun you know?", "Let's not eat fats!","You know what's great food? Salad and vegetables!", "Let's go for a 5 mile walk!","I love burger & pizza too but let's avoid it."];
+  var ob = ["Watch out for yourself! ","Be careful!", "Your heart is sad, let's go workout!", "Diet is the very sexy!", "Bro! Go workout!"];
 
+  var x = Math.floor(Math.random()*6);
+  
   if(bmi<18.6){
     document.getElementById("result").innerHTML = bmi;
     document.getElementById("result").style.color = "yellow";
-    document.getElementById("comment").innerHTML = "Under weight, eat healthy!";
+    document.getElementById("comment").innerHTML = uw[x];
   }
   else if(bmi>=18.6 && bmi<=24.9){
     document.getElementById("result").innerHTML = bmi;
     document.getElementById("result").style.color = "green";
-    document.getElementById("comment").innerHTML = "Wow! you are so fit!";
+    document.getElementById("comment").innerHTML = fit[x];
+    
   }
   else if(bmi>24.9 && bmi<=29.9){
     document.getElementById("result").innerHTML = bmi;
     document.getElementById("result").style.color = "red";
-    document.getElementById("comment").innerHTML = "Diet and workout is fun you know?";
+    document.getElementById("comment").innerHTML = ow[x];
   }
   else{
     document.getElementById("result").innerHTML = bmi;
     document.getElementById("result").style.color = "#BE1600";
-    document.getElementById("comment").innerHTML = "Watch out for yourself! ";
+    document.getElementById("comment").innerHTML = ob[x];
   }
 
   
